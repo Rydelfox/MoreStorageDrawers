c617fa162954fca1d0556eded05e6d788f781b56
Rydelfox <rydelfox@gmail.com>
2022-03-05 21:04:11 -0500

    Nature's Aura Support
    Add Nature's Aura's Ancient Wood

--- src/main/java/com/rydelfox/morestoragedrawers/block/EnumMod.java
+++ src/main/java/com/rydelfox/morestoragedrawers/block/EnumMod.java
@@ -11,7 +11,8 @@ public enum EnumMod implements IStringSerializable {
     ASTRALSORCERY("astralsorcery", EnumVariant.ASTRALSORCERY_INFUSED),
     BOTANIA("botania", EnumVariant.BOTANIA_LIVINGWOOD),
     EIDOLON("eidolon", EnumVariant.EIDOLON_POLISHED),
-    HEXBLADES("hexblades", EnumVariant.HEXBLADES_DARK_POLISHED);
+    HEXBLADES("hexblades", EnumVariant.HEXBLADES_DARK_POLISHED),
+    NATURESAURA("naturesaura", EnumVariant.NATURESAURA_ANCIENT);
 
     private String id;
     private EnumVariant defaultMaterial;
